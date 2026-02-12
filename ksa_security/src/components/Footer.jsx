b2ac7820f66fd9e85afba3667b3ae6cf08dcb89d
Almendra Pereira <alm.pereira@duocuc.cl>
2026-02-11 21:48:12 -0300

    footer

---
+++ ksa_security/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p>&copy; 2024 KSA Security. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
