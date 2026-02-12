f967d0ffd71b4139d39a5c63e77f1aaa763f1265
Almendra Pereira <alm.pereira@duocuc.cl>
2026-02-11 22:30:33 -0300

    cambios

--- ksa_security/src/App.jsx
+++ ksa_security/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer.jsx'
+import LandingPage from './LandingPage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      </div>
-        
+      <LandingPage />
+      <Footer />
+    </div>
   )
 }
 
--- ksa_security/src/LandingPage.jsx
+++ ksa_security/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Slider } from '../components/Slider';
-import { sliderData } from "../data/SliderData";
+import { Slider } from './components/Slider';
+import { sliderData } from "./data/SliderData";
 
 export const LandingPage = () =>{
     return (
