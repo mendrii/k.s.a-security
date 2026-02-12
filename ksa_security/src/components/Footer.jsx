e4e09f32818f3b1d7a5f77a5c87df057dc9ce9db
Almendra Pereira <alm.pereira@duocuc.cl>
2026-02-11 23:36:19 -0300

    arrglos footer

--- ksa_security/src/components/Footer.jsx
+++ ksa_security/src/components/Footer.jsx
@@ -1,42 +1,55 @@
 import React from 'react';
-import '../style/Footer.css'; // Importante importar tu CSS
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
+import '../style/Footer.css'; 
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaClock } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
         
-        {/* Izquierda */}
+        {/* Izquierda: Logo y Copyright */}
         <div className="footer-left">
-          <div className="footer-logo">Company<span>logo</span></div>
-          <div className="footer-links">
-            <a href="#">Home</a> | <a href="#">Blog</a> | <a href="#">Pricing</a> | <a href="#">About</a> | <a href="#">Faq</a> | <a href="#">Contact</a>
+          <div className="footer-logo">
+            <img 
+              src="logo_ksa.png" 
+              alt="K.S.A SECURITY Logo" 
+              className="footer-brand-img"
+            />
           </div>
-          <p className="footer-company-name">Company Name © 2015</p>
+          <p className="footer-company-name">SEGURIDAD KSA © 2026</p>
         </div>
 
-        {/* Centro */}
+        {/* Centro: Contacto y Horario */}
         <div className="footer-center">
           <div className="footer-contact-item">
             <div className="icon-box"><FaMapMarkerAlt /></div>
-            <p>444 S. Cedros Ave <br /><span>Solana Beach, California</span></p>
+            <p><span>Valparaíso, Chile</span></p>
           </div>
           <div className="footer-contact-item">
             <div className="icon-box"><FaPhoneAlt /></div>
-            <p>+1.555.555.5555</p>
+            <p>+56 958042898</p>
           </div>
           <div className="footer-contact-item">
             <div className="icon-box"><FaEnvelope /></div>
-            <p><a href="mailto:support@company.com" className="email-link">support@company.com</a></p>
+            <p><a href="mailto:soporte@empresa.com" className="email-link">soporte@empresa.com</a></p>
+          </div>
+          <div className="footer-contact-item">
+            <div className="icon-box"><FaClock /></div>
+            <p>Lunes-Sábado: 9:00 - 19:00</p>
           </div>
         </div>
 
-        {/* Derecha */}
+        {/* Derecha: Descripción y Redes */}
         <div className="footer-right">
           <div className="footer-about">
-            <h3>About the company</h3>
-            <p>Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.</p>
+            <h3>ACERCA DE LA EMPRESA</h3>
+            <p>
+              En KSA SECURITY nos especializamos en proteger lo que más valoras. 
+              Somos expertos en la instalación y mantenimiento de sistemas integrales 
+              de seguridad: cámaras de alta definición, cercos eléctricos certificados, 
+              citofonía avanzada, alarmas inteligentes y videoporteros. 
+              Tu tranquilidad es nuestra prioridad.
+            </p>
             <div className="footer-socials">
               <a href="#" className="social-icon"><FaFacebookF /></a>
               <a href="#" className="social-icon"><FaTwitter /></a>
