e2c47e47147e6759d5095da1bb73aacc3d227a10
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-02-20 17:09:04 -0300

    vite config

--- ksa_security/vite.config.js
+++ ksa_security/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
